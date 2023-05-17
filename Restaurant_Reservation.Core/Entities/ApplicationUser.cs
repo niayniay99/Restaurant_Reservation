329d8342e3b8ebfa61d742ee6f929559a4b3a2fd
nilay_d <niayniay99@gmail.com>
2023-05-17 15:06:12 +0530

    Add project files.

---
+++ Restaurant_Reservation.Core/Entities/ApplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_Reservation.Core.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+        public DateTime JoinedDate { get; set; }
+
+        [NotMapped]
+        public string Role { get; set; }
+        public bool isSubscriber { get; set; }
+
+    }
+}
